56fd7460b36ec2e7640d64968e6fa337afee528b
Grzegorz Witek <arnvald.to@gmail.com>
2015-07-05 21:17:47 +0800

    Copied files from Discourse repository
    I copied files, except for rake task, from Discourse
    (I hope I didn't save any of changes I did when
    testing gem!) and tested it locally - everything works.

--- lib/autospec.rb
+++ lib/autospec.rb
@@ -1,4 +1,14 @@
 require "autospec/version"
+require 'autospec/manager'
+require 'autospec/base_runner'
+require 'autospec/formatter'
+require 'autospec/manager'
+require 'autospec/qunit_runner'
+require 'autospec/rspec_runner'
+require 'autospec/reload_css'
+require 'autospec/reload_css'
+require 'autospec/simple_runner'
+require 'autospec/spork_runner'
 
 module Autospec
   # Your code goes here...
